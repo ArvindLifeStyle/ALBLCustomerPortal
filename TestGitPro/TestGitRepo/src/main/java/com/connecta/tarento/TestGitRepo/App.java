be0b0bd3b911dcc64c0e0c4dbd2544913fab70b6
Vishnukanth Rachamalla <vishnukanth600@gmail.com>
2017-10-27 14:46:57 +0530

    Testing git repository

---
+++ TestGitPro/TestGitRepo/src/main/java/com/connecta/tarento/TestGitRepo/App.java
@@ -0,0 +1,13 @@
+package com.connecta.tarento.TestGitRepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
