a8092cb27b313ecb95351b1bc292e41a79a6bbcc
Vishnukanth Rachamalla <vishnukanth600@gmail.com>
2017-11-05 12:11:41 +0530

    JUnit Test Folder

---
+++ core/user_mgmt/src/test/java/com/arvind/customerPortal/jUnit/FirstTest.java
@@ -0,0 +1,14 @@
+package com.arvind.customerPortal.jUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirstTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
