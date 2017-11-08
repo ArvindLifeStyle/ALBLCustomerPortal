e630cf12e3441134b812d3b350e06fee71739c76
Joydeep Jana <joydeep.jana@arvind.local>
2017-11-08 15:49:35 +0530

    removed junk code from RequestValidation

--- core/user_mgmt/src/main/java/com/arvind/customerPortal/request/validator/RequestValidatation.java
+++ core/user_mgmt/src/main/java/com/arvind/customerPortal/request/validator/RequestValidatation.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.arvind.customerPortal.domain.BusUser;
 import com.arvind.customerPortal.exceptions.DataNotFoundException;
 import com.arvind.customerPortal.model.LoginRequest;
