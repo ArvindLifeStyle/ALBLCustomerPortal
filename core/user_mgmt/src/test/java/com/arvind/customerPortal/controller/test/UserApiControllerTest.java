9488d280dc0927fe994101b959a0a2cf7ad0d699
Vishnukanth Rachamalla <vishnukanth600@gmail.com>
2017-11-13 11:52:29 +0530

    added Test cases for User APi Controller

--- core/user_mgmt/src/test/java/com/arvind/customerPortal/controller/test/UserApiControllerTest.java
+++ core/user_mgmt/src/test/java/com/arvind/customerPortal/controller/test/UserApiControllerTest.java
@@ -1,15 +1,6 @@
 package com.arvind.customerPortal.controller.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.constraints.NotNull;
-import javax.xml.bind.JAXBException;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,7 +10,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.slf4j.Logger;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.validation.BindingResult;
 
@@ -29,7 +19,6 @@ import com.arvind.customerPortal.exceptions.InsufficientAuthoritiesException;
 import com.arvind.customerPortal.exceptions.UserNotFoundException;
 import com.arvind.customerPortal.mapper.LoginRequestToDtoMapper;
 import com.arvind.customerPortal.model.LoginRequest;
-//import com.arvind.customerPortal.model.LoginRequest.RoleEnum;
 import com.arvind.customerPortal.model.LoginResult;
 import com.arvind.customerPortal.model.UserRegister;
 import com.arvind.customerPortal.request.validator.RequestValidatation;
@@ -38,65 +27,44 @@ import com.arvind.customerPortal.security.Validator;
 import com.arvind.customerPortal.service.ExternalRegisterService;
 import com.arvind.customerPortal.service.InternalRegisterService;
 import com.arvind.customerPortal.service.LoginService;
-import com.arvind.customerPortal.service.impl.ExternalRegisterServiceImpl;
-
-/*enum RoleEnum {
-	ADMIN("ADMIN"),
-
-	CUSTOMER("CUSTOMER"),
-
-	ARVIND_BRAND_TEAM("ARVIND BRAND TEAM");
-
-	@NotNull
-	private String value;
-
-	RoleEnum(String value) {
-		this.value = value;
-
-	}*/
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserApiControllerTest {
-	
+
 	@InjectMocks
 	UserApiController userApiController;
-	
-/*	 @Mock
-	 public RoleEnum testAdmin = RoleEnum.ADMIN;*/
+
 
 	@Mock
 	private InternalRegisterService internalRegisterService;
 
 	@Mock
-	private ExternalRegisterServiceImpl externalRegisterServiceImpl;
+	private ExternalRegisterService externalRegisterService123;
 
 	@Mock
 	private TokenParser testtokenParser;
-	
+
 	@Mock
 	private BindingResult result;
-	
-	/*@Mock
-	private RoleEnum roleEnum;*/
 
 	@Mock
 	private Validator testValidator;
-	
+
 	@Mock
 	private LoginRequestToDtoMapper testMapper;
 
 	@Mock
 	private RequestValidatation requestValidatation;
-	
+
 	@Mock
 	private LoginService testLoginService;
-	
-	
+
+
 	/*test request and response objects*/
-	//private LoginRequest testLoginRequestCorrect=new LoginRequest();
 	private LoginResult testLoginResult=new LoginResult();
 	private UserRegister testUserRegister=new UserRegister();
 	LoginRequest testLoginRequestCorrect=new LoginRequest();
+
 	//Pre-Initialize the mock objects
 	@Before
 	public void setUp()
@@ -108,15 +76,12 @@ public class UserApiControllerTest {
 		ReflectionTestUtils.setField(userApiController, "loginservice", testLoginService);
 		ReflectionTestUtils.setField(userApiController, "tokenParser", testtokenParser);
 		ReflectionTestUtils.setField(userApiController, "validator", testValidator);
-		ReflectionTestUtils.setField(userApiController, "externalRegisterService", externalRegisterServiceImpl);
-		//ReflectionTestUtils.setField(testLoginRequestCorrect, "RoleEnum", testAdmin);
 	}
-	
-	/*@Test
+
+	@Test
 	public void testLogin() throws UserNotFoundException
 	{
 		testLoginRequestCorrect.setPassword("test");
-		//testLoginRequestCorrect.setRole(testAdmin);
 		testLoginRequestCorrect.setUsername("testUser");
 		userApiController.doLogin(testLoginRequestCorrect,result);
 		LoginRequestDTO testLoginDto=new LoginRequestDTO();
@@ -128,25 +93,46 @@ public class UserApiControllerTest {
 		Mockito.doNothing().when(requestValidatation).validateLoginRequest(testLoginRequestCorrect);
 		Mockito.when(testMapper.mapLogintoDto(testLoginRequestCorrect)).thenReturn(testLoginDto);
 		Mockito.when(testLoginService.getLoginDetails(testLoginDto)).thenReturn(testLoginResult);
-	}*/
-	
-	/*@Test
-	public void externalRegistration() throws InsufficientAuthoritiesException
-	{
+	}
+
+	@Test
+	public void exRegistration() throws InsufficientAuthoritiesException {		
+		BindingResult errors = null;
 		testUserRegister.setEmail("testemail");
 		testUserRegister.setName("testName");
 		testUserRegister.setPassword("testPw");
 		testUserRegister.setRole("CUSTOMER");
 		Mockito.doNothing().when(requestValidatation).validateUserRegisterRequest(testUserRegister);
 		Mockito.doNothing().when(requestValidatation).isExist(testUserRegister);
-		userApiController.externalRegistration(testUserRegister, errors, "test");
-		//Mockito.doReturn(true).when(testtokenParser.getRole("test"));
+		when(testtokenParser.getRole("token")).thenReturn("ADMIN");
+		when(testValidator.validate("ADMIN", "register_user")).thenReturn(true);
+		when(externalRegisterService123.register(testUserRegister)).thenReturn(true);
+		userApiController.externalRegistration(testUserRegister, errors, "token");
+
+		testUserRegister = null;
+		when(externalRegisterService123.register(testUserRegister)).thenReturn(false);
+		userApiController.externalRegistration(testUserRegister, errors, "token");
+
 	}
-	*/
-	
+
+
 	@Test
-	public void test() {
-		System.out.println("Junit Testing");
+	public void internalRegistration() {
+
+		BindingResult errors = null;
+		testUserRegister.setEmail("testemail");
+		testUserRegister.setName("testName");
+		testUserRegister.setPassword("testPw");
+		testUserRegister.setRole("CUSTOMER");
+		Mockito.doNothing().when(requestValidatation).validateUserRegisterRequest(testUserRegister);
+		Mockito.doNothing().when(requestValidatation).isExist(testUserRegister);			
+		Mockito.when(internalRegisterService.register(testUserRegister)).thenReturn(true);
+		userApiController.internalRegistration(testUserRegister, errors);
+
+		testUserRegister = null;
+
+		Mockito.when(internalRegisterService.register(testUserRegister)).thenReturn(false);
+		userApiController.internalRegistration(testUserRegister, errors);
 	}
 
 }
